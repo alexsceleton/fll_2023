45ceadfaf594ed13e8edf88f086f7cfcf046ab1e
Alexandr Zhukov <alexzzh@gmail.com>
2023-01-28 00:00:07 +0300

    mission 4

--- fll.py
+++ fll.py
@@ -14,7 +14,7 @@ CTRL_CLOCK = 'counterclockwise'
 
 AVG = 60 # Значение серого
 PWR = 50 # Мощность
-G = 0.6 # Коэффицент
+G = 0.5 # Коэффицент
 
 
 def down():
@@ -29,8 +29,14 @@ hub = PrimeHub()
 hub.light_matrix.show_image('HAPPY')
 drive = MotorPair('C', 'D')
 drive.set_motor_rotation(17.6)
-drive.set_default_speed(100)
+drive.set_default_speed(30)
 drive.set_stop_action('brake')
+
+def right():
+    drive.move(8.5, steering=100)
+
+def left():
+    drive.move(8.5, steering=-100)
 
 def a_line(amount=None, black=None):
     hub.light_matrix.write('A')
@@ -71,11 +77,11 @@ def mission_2():
     a_line(25)
     a_line(black=True)
     down()
-    drive.move(5, steering=-100)
+    drive.move(6, steering=-100)
     for r in range(0, 4):
         manipulator.run_to_position(255, CLOCK, 100)
         down()
-    drive.move(5, steering=100)
+    drive.move(6, steering=100)
     print('Elapsed time m2: ', timer.now())
 
 def mission_8():
@@ -83,7 +89,7 @@ def mission_8():
     просмотр телевизора
     '''
     down()
-    drive.move(10, speed=30)
+    drive.move(10)
     drive.move(-10)
     up()
     print('Elapsed time m8: ', timer.now())
@@ -93,12 +99,12 @@ def mission_13():
     фабрика игрушек
     '''
     manipulator.run_to_position(227, CLOCK, 100)
-    drive.move(4, speed=30)
+    drive.move(4)
     manipulator.run_to_position(237, CLOCK, 100)
     sleep(1)
-    drive.move(-4, speed=30)
+    drive.move(-4)
     up()
-    drive.move(2.5, speed=30)
+    drive.move(2.5)
     drive.move(6, steering=100)
     print('Elapsed time m13:', timer.now())
 
@@ -109,11 +115,13 @@ def mission_5():
     a_line(amount=40)
     a_line(black=True)
     drive.move(6)
-    drive.move(8.5, steering=-100)
+    left()
     drive.move(-2)
     down()
     drive.move(-3)
     up()
+    drive.move(7)
+    right()
     print('Elapsed time m5:', timer.now())
 
 def mission_9():
@@ -123,23 +131,38 @@ def mission_9():
     drive.move(3)
     a_line(42)
     a_line(black=True)
-    drive.move(-3.5, speed=30)
-    drive.move(8.5, steering=-100, speed=30)
+    drive.move(-3.5)
+    left()
     down()
-    drive.move(5, speed=30)
+    drive.move(5)
     manipulator.run_to_position(255, CLOCK)
-    drive.move(3, speed=30)
+    drive.move(3)
     manipulator.run_to_position(263, CLOCK)
-    drive.move(9, speed=30)
+    drive.move(9)
     #drive.move(-11)
     up()
-    drive.move(4, speed=30) 
-    drive.move(9, steering=100, speed=30)
-    drive.move(-8, speed=30)
+    drive.move(4)
+    drive.move(9, steering=100)
+    drive.move(-8)
     down()
     print('Elapsed time m9:', timer.now())
 
+def mission_4():
+    a_line(black=True)
+    drive.move(4)
+    left()
+    drive.move(10)
+    down()
+    drive.move(-22)
+    drive.move(4.5, steering=100)
+    manipulator.run_to_position(235)
+    drive.move(30)
+    down()
+    drive.move(-10)
+    drive.move(10, steering=100)
+    drive.move(100)
 
 # mission_2()
 # mission_13()
-mission_9()
+# mission_5()
+mission_4()
